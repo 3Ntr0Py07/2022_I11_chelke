8a504a5a9e53fe4342fdaf99b4884c5d9c14d4eb
Chaossplitter <cosimoh@icloud.com>
2022-01-26 20:43:53 +0100

    Update Zahlenreihe.py

--- Zahlenreihe.py
+++ Zahlenreihe.py
@@ -1,20 +1,36 @@
 #Rechner für Fibonacci Reihe
+from logging import root
 from tkinter import * 
 from tkinter import ttk
+import numpy
+
+class Error(Exception):
+    pass
+
+
+class VError(Error):
+    pass
 
 LARGE_FONT= ("Verdana", 12)
 NORM_FONT = ("Helvetica", 10)
 SMALL_FONT = ("Helvetica", 8)
 
-def popup(msg,ttl,y,x):
-    popup = Tk()
-    popup.wm_title(ttl)
-    label = ttk.Label(popup, text=msg, font=NORM_FONT)
-    label.config(width=20)
-    label.pack(side="top", fill="x", pady=20)
-    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-    B1.pack()
-    popup.mainloop()
+def popup(msg,ttl):
+    root = Tk()
+    root.wm_title(ttl)
+    label = ttk.Label(root, text=msg, font=NORM_FONT)
+    hs = label.winfo_screenheight()
+    y = hs*0.1
+    h = (hs/2)-(y/2)
+    ws = label.winfo_screenwidth()
+    x = ws*0.2
+    w = (ws/2)-(x/2)
+    root.geometry('%dx%d+%d+%d' % (x, y, w ,h ))
+    label.place(relx = 0.5, rely = 0.35, anchor = 'center')
+    B1 = ttk.Button(root, text="Okay", command = root.destroy)
+    B1.place(relx = 0.5, rely = 0.7, anchor = 'center')
+    root.attributes('-topmost', True)
+    root.mainloop()
 
 def menu():
     print('''Menü 
@@ -23,9 +39,14 @@ def menu():
     3: Tabelle & Diagramm
     4: Vorschrift
     5: Zahlwert''')
-    try:
-        m = int(input())
-    except:
-        popup('Eingabe ist kein ganzzahlieger Wert von 1 bis 5','Fehler',0,0)
+    while Running:
+        try:
+            m = int(input())
+            if m < 1: raise VError
+            if m > 5: raise VeError
+        except VError:
+            popup('Die Eingabe muss zwischen 1 und 5 liegen','Value Error')
+        except ValueError:
+            popup('Eingabe muss eine Natürliche Zahl sein','Type Error')
 
 menu()
